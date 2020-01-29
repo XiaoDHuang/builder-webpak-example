e759485eaad57b50baac756aa70c298079d04bdf
XiaoDHuang <1156299758@qq.com>
2020-01-29 20:31:50 +0800

    modify webpack-base-test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 describe('webpack.base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base');
     it('entry', () => {
-        // assert.equal(baseConfig.entry.index, '/Users/mac/project/webpack/chapter01/my-project/builder-webpack/test/smoke/template/src/index/index.js');
-        //assert.equal(baseConfig.entry.search, '/Users/mac/project/webpack/chapter01/my-project/builder-webpack/test/smoke/template/src/search/index.js');
+        assert.equal(baseConfig.entry.index.indexOf('/test/smoke/template/src/index/index.js') > - 1, true);
+        assert.equal(baseConfig.entry.search.indexOf('/test/smoke/template/src/search/index.js') > -1, true);
     }); 
 });
